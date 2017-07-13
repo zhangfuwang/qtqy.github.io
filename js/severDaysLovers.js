b763f06ec2c0f25496dc11d243a4702c2d2196a6
zhangfuwang <zhang_xu6941@163.com>
2017-07-13 13:49:02 +0800

    qtqy

---
+++ js/severDaysLovers.js
@@ -0,0 +1,16 @@
+(function(glabal,undefined){
+
+    var tools = {
+
+        setFontSize:function(size){
+            var oHtml = document.querySelector("html");
+            var width = oHtml.getBoundingClientRect().width;
+            oHtml.style.fontSize = width/size + "px";
+        }
+
+    }
+
+    glabal.tools = tools;
+
+})(window,undefined);
+
